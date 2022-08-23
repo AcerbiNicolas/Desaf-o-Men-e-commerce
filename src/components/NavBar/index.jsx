1e938d711312f02434cc257ed040886ecdfafb55
Acerbi Nicolas <nicoacerbi4@gmail.com>
2022-08-22 22:37:38 -0300

    Arreglo de errores

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,12 +2,15 @@
 const NavBar = () => {
     return(
         <nav class="container">
-            <div class="row align-items-start" class="blockquote">
-                <h2>MERCADO GLOBAL</h2>
-                <a href="#" class="link-dark col btn">Inicio</a>
-                <a href="#" class="link-dark col btn" > Articulos</a>
-                <a href="#" class="link-dark col btn"> Ayuda</a>
-                <a href="#" class="link-dark col btn"> Somos</a>
+            <div class="row align-items-start">
+                <blockquote class="blockquote">
+                    <h2>MERCADO GLOBAL</h2>
+                    <a href="#" class="link-dark col btn">Inicio</a>
+                    <a href="#" class="link-dark col btn" > Articulos</a>
+                    <a href="#" class="link-dark col btn"> Ayuda</a>
+                    <a href="#" class="link-dark col btn"> Somos</a>
+                </blockquote>
+                
             </div>
             
             
