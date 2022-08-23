f1295611e5fd716c7c71abcb36377880c62db100
Acerbi Nicolas <nicoacerbi4@gmail.com>
2022-08-22 22:32:59 -0300

    Completado

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,19 @@
+
+const NavBar = () => {
+    return(
+        <nav class="container">
+            <div class="row align-items-start" class="blockquote">
+                <h2>MERCADO GLOBAL</h2>
+                <a href="#" class="link-dark col btn">Inicio</a>
+                <a href="#" class="link-dark col btn" > Articulos</a>
+                <a href="#" class="link-dark col btn"> Ayuda</a>
+                <a href="#" class="link-dark col btn"> Somos</a>
+            </div>
+            
+            
+        </nav>
+        
+    )
+}
+
+export default NavBar;
